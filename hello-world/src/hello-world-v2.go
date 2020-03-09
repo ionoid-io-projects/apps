5f3577bf626974294991b5498170dcbd6310c473
Djalal Harouni <tixxdz@opendevices.io>
2020-03-09 10:51:46 +0100

    hello-world: add sources
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

---
+++ hello-world/src/hello-world-v2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+        version = "2"
+)
+
+func main() {
+	/* Clean up on ctrl-c */
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
+
+        ver := os.Getenv("version")
+        if version == "" {
+                version = ver
+        }
+
+	for {
+		fmt.Printf("Hello-world App - Deployment Version %s\n", version)
+		time.Sleep(time.Second * 3)
+	}
+}
